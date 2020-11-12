32a691d48c950d5048741d29878304f7c920d84a
Vitor <vitor.cutri@hotmail.com>
2020-11-12 15:49:57 -0300

    O que eu mais tenho usado na Generation

---
+++ HabilidadesGeneration.java
@@ -0,0 +1,12 @@
+package PacoteJava;
+
+public class HabilidadesGeneration {
+	public static void main(String args[]) {
+		String texto;
+		
+		System.out.println("A habilidade que eu mais usei pra chegar até aqui foi a comunicação,\n porque todos"
+				+ " os dias expresso minhas idéias e comunico com minha equipe.\n"
+				+ "Tenho buscado a mentalidade de crescimento frequentemente,\n porque é preciso deixar a zona de conforto e acreditar"
+				+ "não só na minha inteligência,\n mas principalmente no meu esforço");
+	}
+}
